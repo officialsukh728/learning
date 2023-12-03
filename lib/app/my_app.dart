ad3e5dd3c02ec4de02ab335efdf87cd9c805e46c
Sukh <offcial@User.Com>
2023-12-03 19:27:38 +0530

    add

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -3,6 +3,7 @@ import 'package:learning/views/home.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -5,7 +5,7 @@ class CalcultorView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return  const Column(
       children: [
         TextField(
           decoration: InputDecoration(

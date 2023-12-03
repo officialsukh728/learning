06fe8e85b12c304d42ed1c03217d34d353b14605
Sukh <offcial@User.Com>
2023-12-03 19:38:09 +0530

    add

--- lib/helper.dart
+++ lib/helper.dart
@@ -1,3 +1,9 @@
 import 'dart:io';
 
 void printLog(Object? object) => stdout.write(object);
+
+/// git add .
+/// git commit -m "add"
+/// git pull
+/// git push origin
+/// git add . && git commit -m "add" && git push origin

796aad923efcc5aeeb3802418829375c7c8f33c5
Sukh <offcial@User.Com>
2023-12-04 12:17:14 +0530

    >gg>

--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -11,22 +11,55 @@ class CalcultorView extends StatefulWidget {
 class _CalcultorViewState extends State<CalcultorView> {
   int a = 0;
   int b = 0;
-  int c = 0;
+  num c = 0;
   final displayOneController = TextEditingController();
   final displayTwoController = TextEditingController();
-@override
+
+  late final AppLifecycleListener _listener;
+
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    displayOneController.text=a.toString();
-    displayOneController.text=b.toString();
+    displayOneController.text = a.toString();
+    displayOneController.text = b.toString();
+    _listener = AppLifecycleListener(
+        onShow: _onShow,
+        onHide: _onHide,
+      onResume: _onResume,
+      onDetach: _onDetach,
+      onInactive: _onInactive,
+      onPause: _onPause,
+      onRestart: _onRestart,
+      onStateChange: _onStateChange,
+        );
   }
+  void _onShow()=> print("onShow called");
+  void _onHide()=>print("onHind called");
+  void _onResume()=>print("onResume called");
+  void _onDetach()=>print("onDetach called");
+  void _onInactive()=>print("onInactive called");
+  void _onPause()=>print("onPause called");
+  void _onRestart()=>print("onRestart called");
+  void _onStateChange(AppLifecycleState){
+    print("onStatechange called with state: $State");
+  }
+
+
+  @override
+void dispose() {
+    displayTwoController.dispose();
+    displayOneController.dispose();
+    _listener.dispose();
+    super.dispose();
+
+}
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(32.0),
       child: Column(
-        children: [
+        children: <Widget>[
           Calculatordisplay(
               hint: "Enter first number ", controller: displayOneController),
           const SizedBox(
@@ -49,26 +82,64 @@ class _CalcultorViewState extends State<CalcultorView> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               FloatingActionButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    c = num.tryParse(displayOneController.text)! +
+                        num.tryParse(displayTwoController.text)!;
+                  });
+                },
                 backgroundColor: Colors.deepOrange,
                 child: const Icon(Icons.add),
               ),
               FloatingActionButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    c = num.tryParse(displayOneController.text)! -
+                        num.tryParse(displayTwoController.text)!;
+                  });
+                },
                 backgroundColor: Colors.deepOrange,
                 child: const Icon(CupertinoIcons.minus),
               ),
               FloatingActionButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    c = num.tryParse(displayOneController.text)! *
+                        num.tryParse(displayTwoController.text)!;
+                  });
+                },
                 backgroundColor: Colors.deepOrange,
                 child: const Icon(CupertinoIcons.multiply),
               ),
               FloatingActionButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    c = num.tryParse(displayOneController.text)! /
+                        num.tryParse(displayTwoController.text)!;
+                  });
+                },
                 backgroundColor: Colors.deepOrange,
                 child: const Icon(CupertinoIcons.divide),
               ),
             ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton.extended(
+            onPressed: () {
+              setState(
+                () {
+                  a = 0;
+                  b = 0;
+                  c = 0;
+                  displayOneController.clear();
+                  displayTwoController.clear();
+                },
+              );
+            },
+            backgroundColor: Colors.deepOrange,
+            label: const Text("clear"),
           ),
         ],
       ),
